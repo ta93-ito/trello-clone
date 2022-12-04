5c2a2c1a0554dc9e4b6db74d0ca0738ab4a0c78a
ta93-ito <tkm.ito@outlook.jp>
2022-12-04 23:22:16 +0900

    implements AddTaskCardButton

--- src/components/task/TaskCards.jsx
+++ src/components/task/TaskCards.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import { AddTaskCardButton } from "./button/AddTaskCardButton";
 import { TaskCard } from "./TaskCard";
 
 export const TaskCards = () => {
+  const [taskCardsList, setTaskCardsList] = useState([
+    {
+      id: "0",
+      draggableId: "item0",
+    },
+  ]);
+
   return (
-    <div>
-      <TaskCard />
-      <AddTaskCardButton />
+    <div className="taskCardsArea">
+      {taskCardsList.map((_) => (
+        <TaskCard />
+      ))}
+      <AddTaskCardButton
+        taskCardsList={taskCardsList}
+        setTaskCardsList={setTaskCardsList}
+      />
     </div>
   );
 };
